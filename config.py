e3980df93d747d22891741b03a3b472fa9c75bb9
adityaddos11 <aagebandehai69@gmail.com>
2024-11-24 15:14:04 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY RAJOWNER90
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7606475212:AAFf4RUxAhOvdOdIqdDlGr4jeC_tK41HZbY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAJOWNER90")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7122900125")
